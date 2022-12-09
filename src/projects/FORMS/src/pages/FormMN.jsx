44d7006d71a74bfc36c4b316dbd2e5e2c28c0cfc
EvGen Gal <genwolk1@gmail.com>
2022-12-09 22:44:48 +0300

    FormMN. хук useToggle

--- src/projects/FORMS/src/pages/FormMN.jsx
+++ src/projects/FORMS/src/pages/FormMN.jsx
@@ -1,14 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
+
+import { useToggle } from '../hooks/useToggle';
 
 import "./FormMN.scss";
 
 const FormMN = () => {
+  const [isVisible, toggleIsVisible] = useToggle(true);
+
   return (
     <div className="FormMN">
       <div className="FormMN__descript">Form Desc</div>
-      <div className="FormMN__content">Form Cont</div>
+      <div className="FormMN__content">
+        <button onClick={toggleIsVisible}>Переключатель</button>
+        {isVisible && (<>
+          <div>Form Const</div>
+        </>)}
+      </div>
     </div>
   );
 };
