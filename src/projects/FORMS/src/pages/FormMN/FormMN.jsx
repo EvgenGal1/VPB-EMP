c7697cf93a9138e07ca674c6f50880939e5dfaea
EvGen Gal <genwolk1@gmail.com>
2022-12-14 23:58:57 +0300

    FormMN. Нач. Удал не нужные эл.,hooks

--- src/projects/FORMS/src/pages/FormMN/FormMN.jsx
+++ src/projects/FORMS/src/pages/FormMN/FormMN.jsx
@@ -1,20 +1,33 @@
 import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 
-import { ToogleFC } from "../../components/ToggleFC";
-import { BooksFC } from "../../components/BooksFC";
-import { books } from "../../data/data";
-
 import "./FormMN.scss";
 
 const FormMN = () => {
+  // объ на основе useForm
+  const {
+    // достаём встроен методы
+    // регистрация полей формы
+    register,
+    // объ со св-ми
+    formState: { errors },
+    // обёртка на кастом обработчиком
+    handleSubmit,
+  } = useForm();
+
+  // свой handle (обработчик)
+  const onSubmit = () => {};
+
   return (
     <div className="FormMN">
-      <div className="FormMN__descript">FormMN</div>
+      <div className="FormMN__descript">
+        <h1>FormMN</h1>
+      </div>
       <div className="FormMN__content">
-        <ToogleFC />
-        <hr />
-        <BooksFC items={books} />
+        {/* Форма. С методом onSubmit с вызовом встр. handleSubmit */}
+        <form onSubmit={handleSubmit}>
+          <input className="primary" type="submit" />
+        </form>
       </div>
     </div>
   );
